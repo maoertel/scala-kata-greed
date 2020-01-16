f05a177ca37783aa5dc5fe38fa2ad3c2c04106b9
Mathias Oertel <e0e9cf7e@opayq.com>
2020-01-16 12:18:05 +0100

    cleanup

--- src/main/scala/Greed.scala
+++ src/main/scala/Greed.scala
@@ -29,21 +29,29 @@ object Rules {
     case _ => 0
   }
 
-  private val triples: SingleRule = dieResult => evalSingleRule(cond = dieResult.amount == 3, score = dieResult.die * 100)
-  private val quadruples: SingleRule = dieResult => evalSingleRule(cond = dieResult.amount == 4, score = dieResult.die * 100 * 2)
-  private val quintuples: SingleRule = dieResult => evalSingleRule(cond = dieResult.amount == 5, score = dieResult.die * 100 * 4)
-  private val sextuples: SingleRule = dieResult => evalSingleRule(cond = dieResult.amount == 6, score = dieResult.die * 100 * 8)
-  private val compoundSingleRules = triples :: quadruples :: quintuples :: sextuples :: Nil
+  private val triples: SingleRule =
+    dieResult => evalSingleRule(cond = dieResult.amount == 3, score = dieResult.die * 100)
+  private val quadruples: SingleRule =
+    dieResult => evalSingleRule(cond = dieResult.amount == 4, score = dieResult.die * 100 * 2)
+  private val quintuples: SingleRule =
+    dieResult => evalSingleRule(cond = dieResult.amount == 5, score = dieResult.die * 100 * 4)
+  private val sextuples: SingleRule =
+    dieResult => evalSingleRule(cond = dieResult.amount == 6, score = dieResult.die * 100 * 8)
+
+  private val singleRules = triples :: quadruples :: quintuples :: sextuples :: Nil
 
   private val compoundOfSingleRules: CompoundRule = valMap =>
     valMap
-      .map { case (dieValue, amount) => compoundSingleRules.map(rule => rule(DieResult(dieValue, amount))).max }
+      .map { case (dieValue, amount) => singleRules.map(rule => rule(DieResult(dieValue, amount))).max }
       .iterator
       .sum
 
-  private val singleOne: CompoundRule = valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(1), valMapSize = valMap.size, amountOfDies = 1, score = 100)
-  private val tripleOne: CompoundRule = valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(1), valMapSize = valMap.size, amountOfDies = 3, score = 1000)
-  private val singleFive: CompoundRule = valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(5), valMapSize = valMap.size, amountOfDies = 1, score = 50)
+  private val singleOne: CompoundRule =
+    valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(1), valMapSize = valMap.size, amountOfDies = 1, score = 100)
+  private val tripleOne: CompoundRule =
+    valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(1), valMapSize = valMap.size, amountOfDies = 3, score = 1000)
+  private val singleFive: CompoundRule =
+    valMap => evalSpecialRule(maybeAmountOfDies = valMap.get(5), valMapSize = valMap.size, amountOfDies = 1, score = 50)
 
   private val straight: CompoundRule = valMap => if (valMap.size == 6) 1200 else 0
   private val threePairs: CompoundRule = { valMap =>
